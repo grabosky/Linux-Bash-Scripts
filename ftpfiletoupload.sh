9ddc40fb2199b79e90f9bb5039e3d8f3fa11a218
Adam Grabowski <48142646+grabosky@users.noreply.github.com>
2019-05-01 15:34:04 -0400

    Update ftpfiletoupload.sh

--- ftpfiletoupload.sh
+++ ftpfiletoupload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Adam Grabowski
 #2019-05-01
-#Bash to script to automate copying file from local disk to remote FTP server and then renaming the file to cam.jpg using ncftp
+#Bash script to automate copying file from local disk to remote FTP server and then renaming the file to cam.jpg using ncftp
 #You need to have ncftp package installed#
 #Can be used for running in specific intervals using crontab
 #It was scripted to work as file ftp upload script in addition to another .sh script which takes a snapshot from RaspberryPi camera seconds before and renames the file to DATE Value (with the same minute)
