22bc2cea342eaebb4e3a517f01ac283a0a050be1
Adam Grabowski <48142646+grabosky@users.noreply.github.com>
2019-08-06 20:34:44 -0400

    Create closeTunnel.sh

---
+++ closeTunnel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Script for closing a SOCKS5 tunnel
+#Automates closing of local port (8125) in ufw firewall, then prints current SSH process ID
+#Adam Grabowski
+#2019-06-01
+
+if sudo ufw deny 8125 && sudo ufw deny 8125/tcp ; then
+    echo "Port 8125 SUCCESSFULLY CLOSED on server side."
+else
+    echo "Port 8125 FAILED to CLOSE on server side."
+fi
+ps aux |grep ssh
+echo "Find PID of the Tunnel process - it is after login name - and type sudo kill number"
