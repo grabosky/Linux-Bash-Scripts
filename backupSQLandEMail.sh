7338bd8ba29296d0170f4d5fb11c8c563eab0272
Adam Grabowski <48142646+grabosky@users.noreply.github.com>
2019-10-06 20:19:54 -0400

    Create backupSQLandEMail.sh

---
+++ backupSQLandEMail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Backups mysql dump + sends it by e-mail
+#Requirement: mutt installed
+
+{
+    _now=$(date +"%Y-%m-%d-%H_%M_%S")
+    _file="backup_01_$_now.sql.gz"
+
+    echo "$(date "+%Y-%m-%d-%H_%M_%S") : Starting work"
+    sudo mysqldump -u DBUSERNAME -pPASSWORD dbname_01 | gzip > /home/user/backups/backup_01_$_now.sql.gz
+    echo "MySQL Backup for Domain" | sudo mutt -a "/home/user/backups/"$_file -s "Backup SQL - "$_now -- emailaddress@gmail.com
+    echo "$(date "+%Y-%m-%d-%H_%M_%S") : Done"
+
+} 2>&1 | tee errors.log
